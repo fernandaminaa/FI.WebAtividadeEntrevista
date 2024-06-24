dde35a0e3ad1cc8aea90ac0cedb71073cb633b29
User <User@DESKTOP-85P0U6A>
2024-06-24 09:08:09 -0300

    Adicionar arquivos de projeto.

---
+++ FI.AtividadeEntrevista/DAL/Clientes/DaoCliente.cs
@@ -0,0 +1,242 @@
+﻿using FI.AtividadeEntrevista.DML;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace FI.AtividadeEntrevista.DAL
+{
+    /// <summary>
+    /// Classe de acesso a dados de Cliente
+    /// </summary>
+    internal class DaoCliente : AcessoDados
+    {
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        internal long Incluir(DML.Cliente cliente)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Nome", cliente.Nome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Sobrenome", cliente.Sobrenome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Nacionalidade", cliente.Nacionalidade));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CEP", cliente.CEP));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Estado", cliente.Estado));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Cidade", cliente.Cidade));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Logradouro", cliente.Logradouro));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Email", cliente.Email));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Telefone", cliente.Telefone));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CPF", cliente.CPF));
+
+            DataSet ds = base.Consultar("FI_SP_IncClienteV2", parametros);
+            long ret = 0;
+            if (ds.Tables[0].Rows.Count > 0)
+                long.TryParse(ds.Tables[0].Rows[0][0].ToString(), out ret);
+            return ret;
+        }
+
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        internal DML.Cliente Consultar(long Id)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Id", Id));
+
+            DataSet ds = base.Consultar("FI_SP_ConsCliente", parametros);
+            List<DML.Cliente> cli = Converter(ds);
+
+            return cli.FirstOrDefault();
+        }
+
+        internal bool VerificarExistencia(string CPF)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CPF", CPF));
+
+            DataSet ds = base.Consultar("FI_SP_VerificaCliente", parametros);
+
+            return ds.Tables[0].Rows.Count > 0;
+        }
+
+        internal List<Cliente> Pesquisa(int iniciarEm, int quantidade, string campoOrdenacao, bool crescente, out int qtd)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("iniciarEm", iniciarEm));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("quantidade", quantidade));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("campoOrdenacao", campoOrdenacao));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("crescente", crescente));
+
+            DataSet ds = base.Consultar("FI_SP_PesqCliente", parametros);
+            List<DML.Cliente> cli = Converter(ds);
+
+            int iQtd = 0;
+
+            if (ds.Tables.Count > 1 && ds.Tables[1].Rows.Count > 0)
+                int.TryParse(ds.Tables[1].Rows[0][0].ToString(), out iQtd);
+
+            qtd = iQtd;
+
+            return cli;
+        }
+
+        /// <summary>
+        /// Lista todos os clientes
+        /// </summary>
+        internal List<DML.Cliente> Listar()
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Id", 0));
+
+            DataSet ds = base.Consultar("FI_SP_ConsCliente", parametros);
+            List<DML.Cliente> cli = Converter(ds);
+
+            return cli;
+        }
+
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        internal void Alterar(DML.Cliente cliente)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Nome", cliente.Nome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Sobrenome", cliente.Sobrenome));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Nacionalidade", cliente.Nacionalidade));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CEP", cliente.CEP));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Estado", cliente.Estado));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Cidade", cliente.Cidade));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Logradouro", cliente.Logradouro));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Email", cliente.Email));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Telefone", cliente.Telefone));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("ID", cliente.Id));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CPF", cliente.CPF));
+
+            base.Executar("FI_SP_AltCliente", parametros);
+        }
+
+
+        /// <summary>
+        /// Excluir Cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        internal void Excluir(long Id)
+        {
+            List<System.Data.SqlClient.SqlParameter> parametros = new List<System.Data.SqlClient.SqlParameter>();
+
+            parametros.Add(new System.Data.SqlClient.SqlParameter("Id", Id));
+
+            base.Executar("FI_SP_DelCliente", parametros);
+        }
+
+        private List<DML.Cliente> Converter(DataSet ds)
+        {
+            List<DML.Cliente> lista = new List<DML.Cliente>();
+            if (ds != null && ds.Tables != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    DML.Cliente cli = new DML.Cliente();
+                    cli.Id = row.Field<long>("Id");
+                    cli.CEP = row.Field<string>("CEP");
+                    cli.Cidade = row.Field<string>("Cidade");
+                    cli.Email = row.Field<string>("Email");
+                    cli.Estado = row.Field<string>("Estado");
+                    cli.Logradouro = row.Field<string>("Logradouro");
+                    cli.Nacionalidade = row.Field<string>("Nacionalidade");
+                    cli.Nome = row.Field<string>("Nome");
+                    cli.Sobrenome = row.Field<string>("Sobrenome");
+                    cli.Telefone = row.Field<string>("Telefone");
+                    cli.CPF = row.Field<string>("CPF");
+                    lista.Add(cli);
+                }
+            }
+
+            return lista;
+        }
+
+        public static bool IsValidCpf(string cpf)
+        {
+            if (cpf == null)
+            {
+                return false;
+            }
+
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+
+            bool allDigitsSame = true;
+            for (int i = 1; i < 11 && allDigitsSame; i++)
+            {
+                if (cpf[i] != cpf[0])
+                {
+                    allDigitsSame = false;
+                }
+            }
+
+            if (allDigitsSame || cpf == "12345678909")
+            {
+                return false;
+            }
+
+            int[] numbers = new int[11];
+
+            for (int i = 0; i < 11; i++)
+            {
+                numbers[i] = int.Parse(cpf[i].ToString());
+            }
+
+            int sum = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                sum += (10 - i) * numbers[i];
+            }
+
+            int result = sum % 11;
+            if (result == 1 || result == 0)
+            {
+                if (numbers[9] != 0)
+                {
+                    return false;
+                }
+            }
+            else if (numbers[9] != 11 - result)
+            {
+                return false;
+            }
+
+            sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += (11 - i) * numbers[i];
+            }
+
+            result = sum % 11;
+            if (result == 1 || result == 0)
+            {
+                if (numbers[10] != 0)
+                {
+                    return false;
+                }
+            }
+            else if (numbers[10] != 11 - result)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
